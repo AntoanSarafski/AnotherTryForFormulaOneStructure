e80d08126a7baf1e071169aa28c756eeb20f0f57
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-05 08:31:44 +0300

    Commit with pilot class.

--- Formula1/Models/Ferrari.cs
+++ Formula1/Models/Ferrari.cs
@@ -8,5 +8,9 @@ namespace Formula1.Models
 {
     public class Ferrari : FormulaOneCar
     {
+        public Ferrari(string model, int horsepower, double engineDisplacement)
+            : base(model, horsepower, engineDisplacement)
+        {
+        }
     }
 }
--- Formula1/Models/Pilot.cs
+++ Formula1/Models/Pilot.cs
@@ -1,4 +1,5 @@
 ﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,50 @@ namespace Formula1.Models
 {
     public class Pilot : IPilot
     {
-        public string FullName => throw new NotImplementedException();
+        private string fullName;
+        private IFormulaOneCar car;
+        private int numberOfWins;
+        private bool canRace;
 
-        public IFormulaOneCar Car => throw new NotImplementedException();
 
-        public int NumberOfWins => throw new NotImplementedException();
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Length < 5) //NullOrEmpty? 
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidPilot, value));
+                }
+                fullName = value;
+            }
+        }
 
-        public bool CanRace => throw new NotImplementedException();
+        
+
+        public IFormulaOneCar Car
+        {
+            get { return car; }
+            private set { car = value; }
+        }
+
+       
+
+        public int NumberOfWins
+        {
+            get { return numberOfWins; }
+            private set { numberOfWins = value; }
+        }
+
+        
+
+        public bool CanRace
+        {
+            get { return canRace; }
+            private set { canRace = value; }
+        }
+
+        
 
         public void AddCar(IFormulaOneCar car)
         {
